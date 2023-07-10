4462d47b088ed824bac9fdf3ca960da6a7a7d720
codefatal <eziofatal@gmail.com>
2023-07-10 09:04:18 +0900

    Create ㅇㅅㅇ.sql

---
+++ ㅇㅅㅇ.sql
@@ -0,0 +1,6 @@
+--select * from user$;
+--alter session set "_ORACLE_SCRIPT"=true;
+--create user scott identified by tiger;
+--create user kh identified by 159236;
+--grant connect, resource, dba to scott, kh;
+
