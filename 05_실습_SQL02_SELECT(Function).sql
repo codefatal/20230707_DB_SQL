7838c3a570547ca3bae52ba65c95bacde650f62c
codefatal <eziofatal@gmail.com>
2023-07-17 17:40:41 +0900

    2-15 rollup null값 제거 쿼리문 추가
    grouping + 공백 오름차순 설정

--- 05_실습_SQL02_SELECT(Function).sql
+++ 05_실습_SQL02_SELECT(Function).sql
@@ -143,3 +143,16 @@ select substr(term_no,1,4) as "년도", substr(term_no,5,2) as "학기", round(avg(p
             where student_no = 'A112113'
                 group by rollup(substr(term_no,1,4), substr(term_no,5,2))
                     order by 1;
+                    
+-- rollup을 사용해서 쿼리문 작성(grouping을 이용하여 null 값 공백 치환)
+select 
+case when grouping(substr(term_no,1,4)) = 1 then ' ' else substr(term_no,1,4) end as "년도",
+case when grouping(substr(term_no,5,2)) = 1 then ' ' else substr(term_no,5,2) end as "학기", 
+round(avg(point),1) as "평점"
+    from tb_student
+        left join tb_grade using (student_no)
+            where student_no = 'A112113'
+                group by rollup(substr(term_no,1,4), substr(term_no,5,2))
+                    order by 
+                        case when "년도" = ' ' then 1 else 0 end, "년도", 
+                        case when "학기" = ' ' then 1 else 0 end, "학기";
